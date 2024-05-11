81d27d599d68b4b7815397de79478c4ae54a678c
toki513 <tokitahmidkhan@gmail.com>
2024-05-11 22:26:26 +0600

    Updated

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const container = document.getElementsByClassName("container")
+const container = document.querySelector(".container")
+
+const careers = ["student", "dreamer" ,"web developer","Freelancer"];
+
+container.innerHTML = `
+    <h1>I am a ${careers[2]}</h1>
+`;
