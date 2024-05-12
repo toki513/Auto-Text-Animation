8c9c523dde72bf805c9b1dd0da8d1a4e4ce919ae
toki513 <tokitahmidkhan@gmail.com>
2024-05-12 11:04:55 +0600

    updated

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ let characterIndex = 0;
 updateText();
 
 function updateText(){
+    characterIndex++;
     container.innerHTML = `
     <h1>I am a ${careers[careerIndex].slice(0,characterIndex)} </h1>
     `;
-    characterIndex++;
+    
     if(characterIndex === careers[careerIndex].length){
-        
+        careerIndex++;
+        characterIndex = 0;
+    }
+    if(careerIndex === careers.length){
+        careerIndex = 0;
     }
     setTimeout(updateText, 400);
 }
