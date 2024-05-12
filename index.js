eabd04cf3c59efb0acc66787f4b0d132c5c60bfa
toki513 <tokitahmidkhan@gmail.com>
2024-05-12 11:00:05 +0600

    updated

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const container = document.querySelector(".container")
 const careers = ["student", "dreamer" ,"web developer", "content Creator ","Freelancer"];
 
 let careerIndex = 0;
+let characterIndex = 0;
+updateText();
 
-container.innerHTML = `
-    <h1>I am a ${careers[careerIndex]}</h1>
-`;
+function updateText(){
+    container.innerHTML = `
+    <h1>I am a ${careers[careerIndex].slice(0,characterIndex)} </h1>
+    `;
+    characterIndex++;
+    if(characterIndex === careers[careerIndex].length){
+        
+    }
+    setTimeout(updateText, 400);
+}
